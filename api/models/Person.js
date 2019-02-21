9c7f78de1a9443e6f555357de3b91bb8a6129368
edwardt610 <mingyeung61o@gmail.com>
2019-02-21 15:26:02 +0800

    1st version

---
+++ api/models/Person.js
@@ -0,0 +1,37 @@
+/**
+ * Person.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {   
+    name: {
+      type: "string"
+    },
+    
+    age: {
+      type: "number"
+    },
+    
+    worksFor: {
+      collection: 'User',
+      via: 'supervises'
+    },
+
+    birthDate: {
+      type: 'ref',
+      columnType: 'datetime'
+   },
+
+  },
+
+  getInvalidIdMsg: function (opts) {
+    if (typeof opts.id === "undefined" || isNaN(parseInt(opts.id)))
+    return "Person id not specified or with incorrect type.";
+    return null; // falsy
+   },
+};
+
